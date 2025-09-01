1b80889956a07fb24d33fcc2c49a326040a9629f
arieldelgrande <ariel.delgrande@meltwater.com>
2025-09-01 14:43:56 -0600

    Migrate Terraform configuration from eu-west-1 to us-east-1 and remove obsolete files

--- eu-west-1/staging/ecr.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_ecr_repository" "publish_ecr" {
-  name                 = "publish"
-  image_tag_mutability = "MUTABLE"
-
-  image_scanning_configuration {
-    scan_on_push = true
-  }
-}
-# testing comment
