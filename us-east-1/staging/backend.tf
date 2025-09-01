e10189df7e4398bab17b76bf3d56a814e26f12b6
arieldelgrande <ariel.delgrande@meltwater.com>
2025-09-01 15:30:29 -0600

    Fix formatting in backend.tf file

--- us-east-1/staging/backend.tf
+++ us-east-1/staging/backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket         = "spookyd-infra"      
+    bucket         = "spookyd-infra"
     key            = "us-east-1/staging/terraform.tfstate"
     region         = "us-east-1"
     encrypt        = true
-    dynamodb_table = "spookyd-infra-lock-table"        
+    dynamodb_table = "spookyd-infra-lock-table"
   }
 }
